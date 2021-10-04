e0d28796b6c3d872e351371f5672aa82c351d38f
javaquero <joseantonio.vaquero@ezentis.com>
2021-10-04 14:16:48 +0200

    * Uso de v-for para generar una lista y :key para establecer un identificador en cada elemento de la lista

--- www/main.js
+++ www/main.js
@@ -3,7 +3,18 @@ const app = Vue.createApp({
     return {
       producto: "Calcetines",
       imagen: "./assets/images/socks_green.jpg",
-      inventario: 0
+      inventario: 100,
+      detalles: ['50% Algodon', '20% Lana', '30% Polyester'],
+      tipos: [
+        {
+          id: 3334,
+          color: 'Verde'
+        },
+        {
+          id: 3335,
+          color: 'Azul'
+        },
+      ]
     }
   }
 })
